6c474295ab611e6a3bf2cc2ba0522181381c2dfd
jppreti <jppreti@gmail.com>
2016-05-18 11:06:20 -0400

    Adicionados Exemplos de JavaFX.

---
+++ gui/src/main/java/gui/javafx/ExemploFXML.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates and open the template in
+ * the editor.
+ */
+package gui.javafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ExemploFXML implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Voce clicou no botao!");
+        label.setText("Ola Mundo!");
+    }
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
