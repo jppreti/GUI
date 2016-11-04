0837636f510c08842f2b6dc146dcf7babb1d6f18
jppreti <jppreti@gmail.com>
2016-11-04 16:01:13 -0300

    PaintBrush Pencil.

--- gui/src/main/java/gui/javafx/PaintBrush.java
+++ gui/src/main/java/gui/javafx/PaintBrush.java
@@ -5,12 +5,17 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
 
 public class PaintBrush extends Application {
+
+    Polyline poly = new Polyline();
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -20,10 +25,23 @@ public class PaintBrush extends Application {
 
         Pane tela = new Pane();
 
+        tela.getChildren().add(poly);
+
         scene.setOnMouseDragged(e -> {
-                tela.getChildren().add(new Circle(e.getX(), e.getY(), 2));
+            tela.getChildren().add(new Circle(e.getX(),e.getY(),4,new Color(Math.random(),Math.random(),Math.random(),1)));
+            //poly.getPoints().addAll(new Double[]{e.getX(), e.getY()});
         });
 
+        scene.setOnMouseReleased(e->{
+            poly = new Polyline();
+            //DropShadow ds = new DropShadow();
+            //ds.setOffsetY(3.0);
+            //ds.setOffsetX(3.0);
+            //ds.setColor(Color.BLUE);
+            //poly.setEffect(ds);
+            poly.setStrokeWidth(Math.random()*10);
+            tela.getChildren().add(poly);
+        });
         root.getChildren().add(tela);
 
         stage.setScene(scene);
